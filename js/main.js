8b390344a5dae20b228090750ddb9f0de13bd9d8
Edward Alonso Morales Russi <emorales@sistran.com.co>
2025-07-27 20:56:20 -0500

    Animaciones y favicon

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("menu.html")
-      .then((res) => res.text())
-      .then((data) => {
-        document.getElementById("menu-container").innerHTML = data;
-      });
-  });
-  
+  fetch("menu.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("menu-container").innerHTML = data;
+
+      // Activar IntersectionObserver después de cargar el menú
+      observeSections();
+    });
+
+  function observeSections() {
+    const sections = document.querySelectorAll(".fade-in-section");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+          }
+        });
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+  }
+});
